6cc8a33d2a6e0d2387f81021d5f9f1600167e872
Rick G <lacunainc04@gmail.com>
2023-08-24 19:48:46 -0700

    Update labels to finalized content

--- annotate.py
+++ annotate.py
@@ -3,21 +3,23 @@ import time
 from utils.filemanager import FileManager
 
 instructions = {
-    'Low Lunge': [
-        'come into a low lunge',
-        'bring one leg back one leg forward with your fingertips underneath your shoulders on the mat',
-        'make sure that your feet are hips-width in distance and that your front leg shin is in a nice straight line over the top of the front foot',
-        'with the ball of the back foot stacked underneath the heel put a little bend in your back leg knee'
+    "Low Lunge": [
+        "come into a low lunge",
+        "bring one leg back one leg forward with your fingertips underneath your shoulders on the mat",
+        "make sure that your feet are hips-width in distance and that your front leg shin is in a nice straight line over the top of the front foot",
+        "with the ball of the back foot stacked underneath the heel put a little bend in your back leg knee"
     ],
-    'Crescent Pose': [
-        'up over your pelvis from here lift your',
-        'lower belly and draw your ribs in begin',
-        'to straighten your back leg by pressing the heel back and lifting the inner',
-        'thigh squeeze your inner thighs together',
-        'your arms up towards the sky as you',
-        'inhale lengthen through the sides of your waist and lift your back ribs as you exhale draw your front ribs down and',
-        'palms to touch and gaze up towards your hands'
-    ],
+    "Crescent Pose": [
+        "place your hands on your front leg knee",
+        "press your torso up over your pelvis",
+        "lift your lower belly and draw your ribs in",
+        "begin to straighten your back leg by pressing the heel back and lifting the inner thigh",
+        "squeeze your inner thighs together",
+        "raise your arms up towards the sky",
+        "as you inhale lengthen through the sides of your waist",
+        "lift your back ribs as you exhale",
+        "bring palms to touch and gaze up towards your hands"
+    ]
 }
 
 def translate_pose_into_number(pose_name):
